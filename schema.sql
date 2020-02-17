eb37d8e60b9ba2f00ef9a41097db8ad627d186e3
Ryan Thomas <rythomas2525@gmail.com>
2020-02-16 18:52:39 -0700

    schema done

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL auto_increment,
+  [name] VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE [role] (
+  id INT NOT NULL auto_increment,
+  title VARCHAR(30) NOT NULL,
+  Salary DECIMAL(6) NOT NULL,
+  department_id INT NOT NUll,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT Null,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM [role];
+SELECT * FROM employee;
